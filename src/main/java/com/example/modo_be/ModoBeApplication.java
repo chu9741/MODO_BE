0b0736694cdb231a54351981fee485639cc8ab7a
chu9741 <chu9741@gmail.com>
2023-03-27 23:01:38 +0900

    setiings

---
+++ src/main/java/com/example/modo_be/ModoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.modo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/modo_be/ModoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
